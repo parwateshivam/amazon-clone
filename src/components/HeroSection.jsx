ef30fd4fd0ef527853ae0457033e4979598cac46
shivamparwate <shivamparvate61@gmail.com>
2025-01-18 13:29:34 +0530

    I have added hero section background image

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../images/hero_section.jpg'
+const HeroSection = () => {
+      return (
+            <>
+                  <div className='w-full'>
+                        <img src={Hero} alt="" />
+                  </div>
+            </>
+      )
+}
+
+export default HeroSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Navbar2 from '../components/Navbar2'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Navbar2/>
+      <HeroSection/>
       <Footer/>
     </div>
   )
